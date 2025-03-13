a5f2527f632dda6efc285f9b2e181715dacadaca
akiyama <daniilsav2000@yandex.ru>
2025-03-14 01:25:35 +0400

    feat: set up non-protected routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,47 @@
-import { ConstructorPage } from '@pages';
+import { ConstructorPage, Feed, NotFound404 } from '@pages';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+
 import '../../index.css';
 import styles from './app.module.css';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
 
-import { AppHeader } from '@components';
+const App = () => {
+  const location = useLocation();
+  const background = location.state?.background;
+  const navigate = useNavigate();
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <Routes location={background || location}>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/feed/:number' element={<OrderInfo />} />
+        <Route path='/ingredients/:id' element={<IngredientDetails />} />
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+      {background && (
+        <Routes>
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal title='' onClose={() => navigate(-1)}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title='' onClose={() => navigate(-1)}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+        </Routes>
+      )}
+    </div>
+  );
+};
 
 export default App;
